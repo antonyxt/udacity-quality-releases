9e26277d4e7cbb418444b8a4685dde68adc42c27
Thomas <antony.a.xavier@accenture.com>
2025-11-16 09:35:51 +0530

    use init to supply values

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -1,17 +1,8 @@
 provider "azurerm" {
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
   features {}
 }
 terraform {
-  backend "azurerm" {
-    storage_account_name = "tfstate85372167"
-    container_name       = "tfstate"
-    key                  = "at.quality.terraform.tfstate"
-    #access_key           = "" $env:ARM_ACCESS_KEY="secret"
-  }
+  backend "azurerm" {}
 }
 module "resource_group" {
   source         = "../../modules/resource_group"
