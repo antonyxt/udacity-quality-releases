39f2a1b7a05eb259b51c50811c1a7b4bcb4d8315
Thomas <antony.a.xavier@accenture.com>
2025-11-21 08:56:38 +0530

    update install script

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -36,9 +36,8 @@ resource "azurerm_linux_virtual_machine" "test_agent" {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-  custom_data = base64encode(
-    templatefile("${path.module}/dependency_install.sh")
-  )
+  custom_data = base64encode(templatefile("${path.module}/agent_install.sh", {
+  }))
   source_image_id = var.packer_image_id
 
   tags = {
